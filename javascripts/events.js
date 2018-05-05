d617761aff0bc54bff3fce4e6578222921e4dae7
Andy <amillion3@gmail.com>
2018-05-04 20:17:52 -0500

    WIP

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,22 @@
 // Attaching all event listeners
+
+const findSearchMatches = input => {
+
+};
+
+const btnSubmitClicked = () => {
+  const submitButton = $('#user-input').val();
+  findSearchMatches(submitButton);
+};
+
+const btnTimeClicked = e => {
+  const timeButton = $(e.target).closest('.btn');
+  console.log(timeButton);
+  // $('#snagged').append(fishCard);
+  // $(e.target).text('Remove From Basket').removeClass('add').addClass('remove');
+};
+
+module.exports = {
+  btnSubmitClicked,
+  btnTimeClicked,
+};
